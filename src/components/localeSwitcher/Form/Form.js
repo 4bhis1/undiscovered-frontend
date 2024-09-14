12b6c6796564d8ece25c4a41babe1215edc46e1c
Abhishek kumar <abhishek.kumar@daffodilsw.com>
2024-09-14 16:48:07 +0530

    updates

--- src/components/localeSwitcher/Form/Form.js
+++ src/components/localeSwitcher/Form/Form.js
@@ -1,5 +1,12 @@
 import React, {useState} from 'react';
+import {FaUser, FaUserFriends} from 'react-icons/fa';
+import {FaPeopleGroup} from 'react-icons/fa6';
+import {MdOutlineFamilyRestroom} from 'react-icons/md';
 import {Box, Flex, Text, TextField} from '@radix-ui/themes';
+
+import './form.css';
+import BasicDateRangeCalendar from './DateCalendar';
+import Slider from '../../Slider/Slider';
 
 const map = {
   text: TextField.Root,
@@ -40,15 +47,50 @@ const Place = ({formState, updateFormState}) => {
 };
 
 const Date = () => {
-  return (
-    <>
-      <Text>When are you planning to go?</Text>
-      <Search />
-    </>
-  );
+  return <BasicDateRangeCalendar />;
 };
 
-const NumberOfPeopleGoing = () => {};
+const NumberOfPeople = [
+  {title: 'Solo', icon: FaUser},
+  {title: 'Couple', icon: FaUserFriends},
+  {title: 'Friends', icon: FaPeopleGroup},
+  {title: 'Family', icon: MdOutlineFamilyRestroom},
+];
+
+const NumberOfPeopleGoing = ({formState, updateFormState}) => {
+  const [_, updateState] = useState();
+  const value = 'who';
+  return (
+    <div className={'who-container'}>
+      {NumberOfPeople.map(({title, icon}) => {
+        const Icon = icon;
+        const who = formState[value];
+
+        let className = 'people-box';
+
+        if (title === who) {
+          className += ' active';
+        }
+
+        return (
+          <div
+            className={className}
+            key={title}
+            onClick={() => {
+              updateFormState(formState => {
+                formState[value] = title;
+                updateState(title);
+                return formState;
+              });
+            }}>
+            <Icon />
+            {title}
+          </div>
+        );
+      })}
+    </div>
+  );
+};
 const BudgetRange = () => {};
 const ActivitiesYouWant = () => {};
 
@@ -61,7 +103,7 @@ const ComponentIndex = [
 ];
 
 const MainForm = () => {
-  const [sliderCount, updateSliderCount] = useState(0);
+  const [sliderCount, updateSliderCount] = useState(1);
 
   const [formState, updateFormState] = useState({});
 
@@ -69,6 +111,8 @@ const MainForm = () => {
 
   return (
     <Flex direction={'column'}>
+      <Slider totalSteps={ComponentIndex.length} currentStep={sliderCount} />
+
       <Text>Plan Your Next Trip</Text>
 
       <Component formState={formState} updateFormState={updateFormState} />
