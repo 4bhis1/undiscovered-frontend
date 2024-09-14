510ed4925b705eb01cbf092cb980c8bd07f9e3e6
Abhishek kumar <abhishek.kumar@daffodilsw.com>
2024-09-15 03:03:26 +0530

    updates

--- src/screens/MainPage.js
+++ src/screens/MainPage.js
@@ -1,11 +1,16 @@
-import React from 'react'
+import React from 'react';
+import LeftNav from './MainPage/LeftNav';
+import Itinary from './MainPage/Itinary';
+import Map from './MainPage/Map';
 
 const MainPage = () => {
   return (
     <div>
-      
+      <LeftNav />
+      <Itinary />
+      <Map />
     </div>
-  )
-}
+  );
+};
 
-export default MainPage
+export default MainPage;
---
+++ src/screens/MainPage/Itinary.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Itinary = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Itinary
