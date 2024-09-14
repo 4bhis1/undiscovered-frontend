c04ca1944f30853e94c904ce3aa771f9759dd9c8
Amit Kumar Sharma <amit.kr.sharma@daffodilsw.com>
2024-09-15 03:51:17 +0530

    update

--- src/screens/MainPage.js
+++ src/screens/MainPage.js
@@ -3,10 +3,17 @@ import LeftNav from './MainPage/LeftNav';
 import Itinary from './MainPage/Itinary';
 import Map from './MainPage/Map';
 
+const sideNavBarItem = [
+  {label: 'Home'},
+  {label: 'About'},
+  {label: 'Contact'},
+  {label: 'Services'},
+  {label: 'Blogs'},
+];
 const MainPage = () => {
   return (
     <div>
-      <LeftNav />
+      <LeftNav data={sideNavBarItem} />
       <Itinary />
       <Map />
     </div>
