7fd1c49655e35cde7f4ede9c576e47a15d4535ee
Abhishek kumar <abhishek.kumar@daffodilsw.com>
2024-09-14 14:47:05 +0530

    updates

---
+++ src/components/localeSwitcher/Form/Form.js
@@ -0,0 +1,110 @@
+import React, {useState} from 'react';
+import {Box, Flex, Text, TextField} from '@radix-ui/themes';
+
+const map = {
+  text: TextField.Root,
+};
+
+export const Search = () => {
+  const SearchComponent = map['text'];
+  const [text, updateText] = useState();
+
+  return (
+    <SearchComponent
+      value={text}
+      onChange={({target}) => {
+        console.log('🚀 ~ file: Form.js:17 ~ Search ~ props:', target.value);
+        updateText(target.value);
+      }}
+    />
+  );
+};
+
+const HR = (
+  <div
+    style={{
+      height: 2,
+      width: '100%',
+      backgroundColor: '#c0c0c0',
+    }}
+  />
+);
+
+const Place = ({formState, updateFormState}) => {
+  return (
+    <>
+      <Text>Where would you like to go?</Text>
+      <Search />
+    </>
+  );
+};
+
+const Date = () => {
+  return (
+    <>
+      <Text>When are you planning to go?</Text>
+      <Search />
+    </>
+  );
+};
+
+const NumberOfPeopleTraining = () => {};
+const BudgetRange = () => {};
+const ActivitiesYouWant = () => {};
+
+const ComponentIndex = [
+  Place,
+  Date,
+  NumberOfPeopleTraining,
+  BudgetRange,
+  ActivitiesYouWant,
+];
+
+const MainForm = () => {
+  const [sliderCount, updateSliderCount] = useState(0);
+
+  const [formState, updateFormState] = useState({});
+
+  const Component = ComponentIndex[sliderCount];
+
+  return (
+    <Flex direction={'column'}>
+      <Text>Plan Your Next Trip</Text>
+
+      <Component formState={formState} updateFormState={updateFormState} />
+
+      <Flex>
+        {sliderCount > 0 && (
+          <Box
+            width="64px"
+            height="64px"
+            onClick={() => {
+              // if (sliderCount > 0) {
+              updateSliderCount(count => {
+                return count - 1;
+              });
+              // }
+            }}>
+            Prev
+          </Box>
+        )}
+        {sliderCount < ComponentIndex.length - 1 && (
+          <Box
+            width="64px"
+            height="64px"
+            onClick={() => {
+              // if (sliderCount < ComponentIndex.length - 1) {
+              updateSliderCount(count => {
+                return count + 1;
+              });
+              // }
+            }}>
+            Next
+          </Box>
+        )}
+      </Flex>
+    </Flex>
+  );
+};
+
+export default MainForm;
